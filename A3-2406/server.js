b8d1ea5264c6414cf467ed483625d2f6ab52c36a
Nicholas Ellul <nicholas.ellul1@gmail.com>
2018-03-08 19:18:55 -0500

    Design server side code

--- A3-2406/server.js
+++ A3-2406/server.js
@@ -40,31 +40,102 @@ function get_mime(filename) {
 }
 
 let playersConnected = [];
+let leftPaddleStatus = {claimed:false};
+let rightPaddleStatus = {claimed:false};
+let leftScore = 0,
+	rightScore = 0;
+
+function resetScore(){
+	leftScore = 0;
+	rightScore = 0;
+
+	io.emit('leftGoal',JSON.stringify({score:leftScore}));
+	io.emit('rightGoal', JSON.stringify({score:rightScore}));
+}
 
 io.on('connection', function(socket){
 
 	playersConnected.push(socket.id);
 	console.log("there are " + playersConnected.length);
 	console.log("This boi " + socket.id + " connected");
-	
-	socket.broadcast.to(playersConnected[0]).emit('sendMeWherePuckIs'); 
 
-	socket.on('clientSays', function(data){
+
+	socket.on('init', (data) =>{
+
+		if(leftPaddleStatus.claimed == true){
+
+			io.sockets.connected[socket.id].emit('leftPlayerClaimed',
+												 JSON.stringify({claimed:true})); 
+		}
+		if(rightPaddleStatus.claimed == true){
+
+			io.sockets.connected[socket.id].emit('rightPlayerClaimed',// change to just broadcast to one dude
+												 JSON.stringify({claimed:true})); 
+		}
+		if(playersConnected.length > 1) {
+			socket.broadcast.to(playersConnected[0]).emit('sendMeWherePuckIs'); 
+		}
+	});
+
+	socket.on('puckLocation', (data) =>{
+
+
+		io.emit('updatePuck',data);
+	});
+	// Data is true or false
+	socket.on('claimPlayerRight', (data) =>{
+		console.log("Owner : " + socket.id);
+		rightPaddleStatus.owner = socket.id;
+		rightPaddleStatus.claimed = data;
+		io.emit('rightPlayerClaimed',data);
+	});
+	socket.on('claimPlayerLeft', (data) =>{
+		console.log("Owner : " + socket.id);
+		leftPaddleStatus.owner = socket.id;
+		leftPaddleStatus.claimed = data;
+		io.emit('leftPlayerClaimed', data);
+	});
+	socket.on('clientSays', (data) =>{
 		socket.broadcast.emit('serverSays', data);
 	});
-
-	socket.on('puckLocation',function(data){
-		socket.broadcast.emit('updatePuck',data);
+	socket.on('leftPlayerUpdate', (data) =>{
+		socket.broadcast.emit('leftPlayerUpdate',data);
 	});
+	socket.on('rightPlayerUpdate', (data) =>{
 
-	socket.on('disconnect', (reason) => {
-		console.log("This boi " + socket.id + "disconnected");
-		
+		socket.broadcast.emit('rightPlayerUpdate',data);
+	});
+	socket.on('leftGoal',(data) =>{
+		leftScore++;
+		io.emit('leftGoal',JSON.stringify({score:leftScore}));
+
+	});
+	socket.on('rightGoal', (data) =>{
+		rightScore++
+		io.emit('rightGoal', JSON.stringify({score:rightScore}));
+
+	});
+	socket.on('disconnect', (reason) =>{
+		console.log("This boi " + socket.id + " disconnected");
+
+
+		if(leftPaddleStatus.owner == socket.id){
+
+			leftPaddleStatus.claimed = false;
+			resetScore();
+			io.emit('leftPlayerClaimed', false);
+		}
+		if(rightPaddleStatus.owner == socket.id){
+			rightPaddleStatus.claimed = false;
+			resetScore();
+			io.emit('rightPlayerClaimed', false);
+		}
+
 		// Remove this player from our list of players while maintaining queue order
 		for(var i = 0; i < playersConnected.length; i++){
 			if(playersConnected[i] == socket.id) break;
 		}
-		
+
 		for(let j = i; j < playersConnected.length - 1; j++){
 			playersConnected[j] = playersConnected[j+1];
 		}
@@ -76,16 +147,16 @@ io.on('connection', function(socket){
 
 function handler(request, response) {
 	let urlObj = url.parse(request.url, true, false)
-	let receivedData = ""
+	let receivedData = "";
 
 	//attached event handlers to collect the message data
 	request.on("data", function(chunk) {
-		receivedData += chunk
+		receivedData += chunk;
 	})
 
 	//event handler for the end of the message
 	request.on("end", function() {
-		
+
 		if (request.method == "GET") {
 			//handle GET requests as static file requests
 			fs.readFile(ROOT_DIR + urlObj.pathname, function(err, data) {
